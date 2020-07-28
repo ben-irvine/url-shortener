0b3b713d1444819a9e63492d3ecd87073c4d6cf8
Ben Irvine <benji72027@gmail.com>
2020-07-28 15:25:03 +1200

    add front end submit function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-alert("wassup!")
+const request = require('superagent')
+
+const input = document.getElementById('submit')
+
+
+input.addEventListener('click', event => {
+    event.preventDefault()
+    let newURL = document.getElementById('url-input').value
+    request.post(newURL)
+    .then((res) => {
+        console.log(res)
+    })
+})
