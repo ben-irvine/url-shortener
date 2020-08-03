c4dc8b51c1daeb4a9e4d023c17bc8247be6ac6f9
rubin-gower <rubinsemails@gmail.com>
2020-08-03 15:19:04 +1200

    displays new url

--- client/components/Create.jsx
+++ client/components/Create.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import {ShortenUrl} from "../API/api"
 
 class Create extends React.Component {
-
+   
+state = {
+    shortendUrls: [
+    ]
+}
+   
     handleSubmit = () => {
         ShortenUrl(document.getElementById("url-input").value)
         .then(res => {
             console.log("res is ", res)
+            let stuff = {hello: "blah"}
+            this.setState({
+                shortendUrls: [...this.state.shortendUrls, {original: document.getElementById("url-input").value, newUrl: window.location + res.data}]
+               
+            })
         })
     }
 
@@ -18,6 +28,15 @@ class Create extends React.Component {
             <h3>Enter your URL:</h3>
             <input id="url-input" type="text" name="full_url" placeholder="Your URL goes here" />
             <button id="submit" onClick={this.handleSubmit}>Submit</button>
+            <div>
+            {this.state.shortendUrls.length > 0 ? 
+            this.state.shortendUrls.map((elem, i) => {
+                console.log("runing foreach: ", elem)
+                return <div key={i}>old: {elem.original} new:  <a href={elem.newUrl}>{elem.newUrl}</a> </div>
+            })
+            : 
+            "false"}
+            </div>
         </>
     )
     }
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -149,6 +149,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _API_api__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../API/api */ "./client/API/api.js");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -190,9 +202,23 @@ var Create = /*#__PURE__*/function (_React$Component) {
 
     _this = _super.call.apply(_super, [this].concat(args));
 
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      shortendUrls: []
+    });
+
     _defineProperty(_assertThisInitialized(_this), "handleSubmit", function () {
       Object(_API_api__WEBPACK_IMPORTED_MODULE_1__["ShortenUrl"])(document.getElementById("url-input").value).then(function (res) {
         console.log("res is ", res);
+        var stuff = {
+          hello: "blah"
+        };
+
+        _this.setState({
+          shortendUrls: [].concat(_toConsumableArray(_this.state.shortendUrls), [{
+            original: document.getElementById("url-input").value,
+            newUrl: window.location + res.data
+          }])
+        });
       });
     });
 
@@ -210,7 +236,14 @@ var Create = /*#__PURE__*/function (_React$Component) {
       }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
         id: "submit",
         onClick: this.handleSubmit
-      }, "Submit"));
+      }, "Submit"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, this.state.shortendUrls.length > 0 ? this.state.shortendUrls.map(function (elem, i) {
+        console.log("runing foreach: ", elem);
+        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          key: i
+        }, "old: ", elem.original, " new:  ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+          href: elem.newUrl
+        }, elem.newUrl), " ");
+      }) : "false"));
     }
   }]);
 
